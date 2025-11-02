b6d600bdc21b3c97aadce3723d12cfef70715770
thomas-lamb-tech <thomas.lamb.tech@gmail.com>
2025-11-03 06:19:16 +0900

    []

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -4,7 +4,7 @@ import React from 'react';
 const WorkCard = (props) => {
     return (
         <div className="project-card">
-            <img alt="image" src={props.imgsrc}/>
+            <img alt="workphoto" src={props.imgsrc}/>
             <h2 className="project-title">{props.title}</h2>
             <div className="pro-details">
                 <p>{props.text}</p>
